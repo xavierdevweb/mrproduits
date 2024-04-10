b5044208355ddff2b0b626efbde308bb94b4602d
Jeremie Tavares <84427506+JeremieTavares@users.noreply.github.com>
2024-04-10 18:46:17 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,39 +1,33 @@
 <?php
 
-
-use Admin\ProductController\Admin;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Authentication\LoginController;
 use App\Http\Controllers\Authentication\RegisterController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', [HomeController::class, 'index'])->name('home');
-// ==========================================================================================================================================================
-// ADMINISTRATION
+
+// ====================================== ADMINISTRATION ================================================
 Route::prefix('admin')->name('admin.')->middleware('auth', 'role:admin')->group(function() {
-    Route::redirect('/', '/admin/products');
     Route::resource('products', ProductController::class);
 });
 
-// ==========================================================================================================================================================
+// ====================================== AUTHENTICATION ================================================
+Route::prefix('auth')->name('auth.')->group(function() {
+    Route::controller(LoginController::class)->name('login.')->group(function() {
+        Route::get('/login', 'index')->name('index');
+        Route::post('/login', 'login')->name('login');
+        Route::post('/logout', 'logout')->name('logout');
+    });
 
-// ==========================================================================================================================================================
-// AUTHENTIFICATION CLIENT
-
-Route::controller(LoginController::class)->name('auth.login.')->prefix('auth')->group(function() {
-    Route::get('/login', 'index')->name('index');
-    Route::post('/login', 'login')->name('login');
-    Route::post('/logout', 'logout')->name('logout');
+    Route::controller(RegisterController::class)->name('register.')->group(function() {
+        Route::get('/register', 'index')->name('index');
+        Route::post('/register', 'store')->name('store');
+    });
 });
 
-Route::controller(RegisterController::class)->name('auth.register.')->prefix('auth')->group(function() {
-    Route::get('/register', 'index')->name('index');
-    Route::post('/register', 'store')->name('store');
-});
-
-// ==========================================================================================================================================================
+// ==================================== PUBLIC PRODUCTS  ==============================================
 Route::controller(ProductController::class)->name('products.')->prefix('products')->group(function() {
     Route::get('/', 'index')->name('index');
     Route::get('/create', 'create')->name('create');
